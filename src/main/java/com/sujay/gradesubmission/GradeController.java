507af89ce55326da071ad4ea546a2fa04d7c6776
sujay2306 <sujayks007@gmail.com>
2023-01-18 07:01:19 +0530

    added dev dependency

---
+++ src/main/java/com/sujay/gradesubmission/GradeController.java
@@ -0,0 +1,12 @@
+package com.sujay.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradeController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Some-View";
+    }
+}
