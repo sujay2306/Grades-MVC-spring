37dae6f89d793c4c99405c2bdf378b59961468d5
sujay2306 <sujayks007@gmail.com>
2023-01-20 06:17:51 +0530

    added model attributes

--- src/main/java/com/sujay/gradesubmission/GradeController.java
+++ src/main/java/com/sujay/gradesubmission/GradeController.java
@@ -1,12 +1,23 @@
 package com.sujay.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class GradeController {
+
+    List<Grade> studentGrades = Arrays.asList(
+            new Grade("sujay", "portions", "c-"),
+             new Grade("sandyy", "portions", "c-"),
+             new Grade("sujay", "portions", "c-")
+    );
     @GetMapping("/grades")
-    public String getGrades(){
+    public String getGrades(Model model){
+        model.addAttribute("grade",studentGrades);
         return "grades";
     }
 }
